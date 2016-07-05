76ebfeadb8d43355229bee321e660ab428ca3cf9
Jimmy Berlin <berlin.jimmy@gmail.com>
2016-07-05 21:43:50 +0200

    can now build tower that change nav mesh for enemy

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,19 +5,38 @@ public class Controller : MonoBehaviour {
 
 	Camera viewCamera;
 
+	public Transform enemy;
+
 	void Start () {
 		viewCamera = Camera.main;
 	}
 
 	void Update () {
-		// Get position of gameboard we are pointing at.
+		// Get the position of the gameboard we are pointing at.
 		Ray ray = viewCamera.ScreenPointToRay (Input.mousePosition);
 		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
 
 		float rayDistance;
 		if (groundPlane.Raycast (ray, out rayDistance)) {
 			Vector3 point = ray.GetPoint (rayDistance);
+
+			point = PointToTileCoord (point);
+
 			Debug.DrawLine (ray.origin, point, Color.red);
+			print (point);
+
+			if (Input.GetMouseButtonUp (0)) {
+				Transform newTower = Instantiate (enemy) as Transform;
+				newTower.transform.position = point;
+			}
 		}
+	}
+
+	Vector3 PointToTileCoord(Vector3 point) {
+		point.x = Mathf.Floor (point.x);
+		point.y = Mathf.Floor (point.y);
+		point.z = Mathf.Floor (point.z);
+
+		return point;
 	}
 }
