92bad5ca667b608eda89cdde37ef9325233d67c1
Jimmy Berlin <berlin.jimmy@gmail.com>
2016-07-07 21:57:50 +0200

    added a small menu system for demo

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	public GameObject mainMenuObject;
+	public GameObject optionsMenuObject;
+	public GameObject confirmQuitObject;
+
+	void Start() {
+		mainMenuObject.SetActive (true);
+	}
+
+	public void Play() {
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void Quit() {
+		mainMenuObject.SetActive (false);
+		confirmQuitObject.SetActive (true);
+	}
+
+	public void ConfirmQuit() {
+		Application.Quit ();
+	}
+
+	public void CancelQuit() {
+		confirmQuitObject.SetActive (false);
+		mainMenuObject.SetActive (true);
+	}
+
+	public void OptionsMenu() {
+		mainMenuObject.SetActive (false);
+		optionsMenuObject.SetActive (true);
+	}
+
+	public void MainMenu() {
+		optionsMenuObject.SetActive (false);
+		mainMenuObject.SetActive (true);
+	}
+}
