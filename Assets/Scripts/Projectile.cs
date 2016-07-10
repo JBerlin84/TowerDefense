400a24881a4e13c9e32c938c98e82caa0fcd9a07
Jimmy Berlin <berlin.jimmy@gmail.com>
2016-07-10 15:38:03 +0200

    Bullets now hit enemies

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,11 +3,30 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
+	public LayerMask hitMask;
+
 	public float speed;
 	public float damage;
 
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.forward * Time.deltaTime * speed);
+		float distanceToTravel = speed * Time.deltaTime;
+
+		CheckCollision (distanceToTravel);
+
+		transform.Translate (Vector3.forward * distanceToTravel);
+	}
+
+	void CheckCollision(float distanceToTravel) {
+		Ray ray = new Ray (transform.position, transform.forward);
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit, distanceToTravel, hitMask, QueryTriggerInteraction.Collide)) {
+			OnHitObject (hit);
+		}
+	}
+
+	void OnHitObject(RaycastHit hit) {
+		GameObject.Destroy (gameObject);
 	}
 }
