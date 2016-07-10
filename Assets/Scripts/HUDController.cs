073a4a688be298461ff9bec8109833d811fae255
Jimmy Berlin <berlin.jimmy@gmail.com>
2016-07-10 17:41:52 +0200

    started ui

---
+++ Assets/Scripts/HUDController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUDController : MonoBehaviour {
+
+	public Text HP;
+	public Text resources;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int startHP;
+	int hp;
+
+	public int resources;
+
+	// Use this for initialization
+	void Start () {
+		hp = startHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
