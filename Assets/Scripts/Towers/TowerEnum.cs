370c1b2001c51e40cce07480236a57322cd49822
Jimmy Berlin <berlin.jimmy@gmail.com>
2016-07-25 18:21:58 +0200

    refrector for tower reference in popup window

---
+++ Assets/Scripts/Towers/TowerEnum.cs
@@ -0,0 +1,6 @@
+﻿/// <summary>
+/// Enum over all the different tower types.
+/// </summary>
+public enum TowerEnum{
+	none, BasicTower, HeavyAssaultTower, MissileTurret, LaserTower, LightningTower, CruiseLauncher, RailGun, FlameThrower, Sniper
+}
