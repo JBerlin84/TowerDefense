0825cce57d48f38235ce19fc6df15e57eb74c220
Berlin <berlin.jimmy@gmail.com>
2016-08-04 22:30:09 +0200

    Added more turrets, new skyboxes and aesthetics for main menu.

---
+++ Assets/Scripts/Towers/LaserRig.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserRig : Tower {
+
+	// Update is called once per frame
+	protected override void Update () {
+		//TODO: Play idle animation of tower.
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+		mainTurret.transform.Rotate (Vector3.up * Time.deltaTime * idleRotationSpeed);
+
+		if (enemies.Length > 0)
+		{
+			for (int i = 0; i < enemies.Length; i++)
+			{
+				float distance = Vector3.Distance(myPosition, enemies[i].transform.position);
+				if (distance < range)
+				{
+					// Enemy is within distance of turret, aim at enemy
+					Vector3 target = enemies[i].transform.position;
+					target.y = muzzle.transform.position.y;
+					muzzle.LookAt(target);
+					Debug.DrawLine(muzzle.transform.position, target);
+
+					Fire();
+					break;
+				}
+			}
+		}
+	}
+
+	protected override void Fire() {
+		base.Fire ();
+
+		if (Time.time > nextAttackTime)
+		{
+			Projectile newProjectile = Instantiate(projectile, muzzle.position, muzzle.rotation) as Projectile;
+			newProjectile.speed *= speedBonus;
+			newProjectile.damage *= damageBonus;
+			nextAttackTime = Time.time + attackSpeed;
+		}
+	}
+}
